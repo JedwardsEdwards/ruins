eba80c32171de301cc5ca3d6324188d3067aebdf
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-20 20:21:40 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -14,18 +14,18 @@ const sha256 = async (plain) => {
   const encoder = new TextEncoder()
   const data = encoder.encode(plain)
   return window.crypto.subtle.digest('SHA-256', data)
-}
+};
 
 const base64encode = (input) => {
   return btoa(String.fromCharCode(...new Uint8Array(input)))
     .replace(/=/g, '')
     .replace(/\+/g, '-')
     .replace(/\//g, '_');
-}
+};
 
-const hashed = await sha256(codeVerifier)
+const hashed = await sha256(codeVerifier);
 const codeChallenge = base64encode(hashed);
 
-console.log(codeChallenge)
+console.log(codeChallenge);
 
 console.log("done");
