c5d004afe327a3c3ed32845f06a55d5df7e2242e
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-20 18:46:10 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,7 +1,2 @@
 console.log("in test script")
-
-const generateRandomString = (length) => {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const values = crypto.getRandomValues(new Uint8Array(length));
-  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-}
+console.log("done")
