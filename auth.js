0f409cfda6925266e4459de37f50ff9c384365d2
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-20 21:26:47 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -10,21 +10,20 @@ const codeVerifier  = generateRandomString(64);
 
 console.log(codeVerifier)
 
-const sha256 = async (plain) => {
+async function sha256(plain) {
   const encoder = new TextEncoder()
   const data = encoder.encode(plain)
+
   return window.crypto.subtle.digest('SHA-256', data)
 }
 
-const base64encode = (input) => {
-  return btoa(String.fromCharCode(...new Uint8Array(input)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
+function base64urlencode(a){
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(a))
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
 }
 
-const hashed = sha256(codeVerifier)
-const codeChallenge = base64encode(hashed);
+const hashed = await sha256(verifyCode)
+const codeChallenge = base64urlencode(hashed)
 
 console.log(codeChallenge)
 
