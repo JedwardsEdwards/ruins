64b788e0a2825d3c14c2d0088c3ca3284bf40e81
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-20 18:54:49 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,8 +1,13 @@
 console.log("in test script");
-alert("alert");
+
 const generateRandomString = (length) => {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const values = crypto.getRandomValues(new Uint8Array(length));
   return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
+
+const codeVerifier  = generateRandomString(64);
+
+console.log(codeVerifier);
+
 console.log("done");
