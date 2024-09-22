1c3939ad2617a529a0279ab7049d631ef84d5402
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-22 18:00:33 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,21 +9,30 @@ const generateRandomString = (length) => {
 const codeVerifier  = generateRandomString(64);
 
 console.log(codeVerifier)
-
-async function sha256(plain) {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(plain)
-
-  return window.crypto.subtle.digest('SHA-256', data)
+function sha256(plain) { 
+    // returns promise ArrayBuffer
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest('SHA-256', data);
 }
 
 function base64urlencode(a) {
-  return btoa(String.fromCharCode.apply(null, new Uint8Array(a))
-    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''))
+    // Convert the ArrayBuffer to string using Uint8 array.
+    // btoa takes chars from 0-255 and base64 encodes.
+    // Then convert the base64 encoded to base64url encoded.
+    // (replace + with -, replace / with _, trim trailing =)
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(a)))
+        .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 }
 
-const hashed = sha256(codeVerifier)
-const codeChallenge = base64urlencode(hashed)
+async function pkce_challenge_from_verifier(v) {
+    hashed = await sha256(v);
+    base64encoded = base64urlencode(hashed);
+    return base64encoded;
+}
+
+//const hashed = sha256(codeVerifier)
+const codeChallenge = pkce_challenge_from_verifier(codeVerifier)
 
 console.log(codeChallenge)
 
