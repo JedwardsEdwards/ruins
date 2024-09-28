71436437af0203de58b2b7a494eee33a82068cff
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-28 13:23:18 +0100

    Update playback.js

--- playback.js
+++ playback.js
@@ -8,10 +8,39 @@
     const callback = (EmbedController) => {
         console.log(EmbedController);
         document.querySelector('#play-pause').addEventListener('click', () => {EmbedController.togglePlay()});
+        playlistDetails();
     };
     IFrameAPI.createController(element, options, callback);
   };
+  
+  function playlistDetails() {
+    fetch('https://api.spotify.com/v1/playlists/' + playlistId + '/tracks', {
+      headers: {
+        Authorization: 'Bearer ' + access_token,
+      },
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw await response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        //document.getElementById('login').style.display = 'none';
+        //document.getElementById('loggedin').style.display = 'unset';
+        //mainPlaceholder.innerHTML = userProfileTemplate(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        mainPlaceholder.innerHTML = errorTemplate(error.error);
+      });
+  }
 
+  function trackDetailTemplate(data) {
+  };
+  
   const trackIndex = 0;
   const playlistId = "73p0zWLYMp2Rs2Kh3PM5Le";
   })();
