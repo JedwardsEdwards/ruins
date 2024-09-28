2f1001aac4ee9934bcff32302d7fd87b9c6e4e16
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-28 14:32:56 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -42,7 +42,7 @@
         {
           response_type: 'code',
           client_id,
-          scope: 'user-read-private user-read-email user-read-playback-state',
+          scope: 'user-read-private user-read-email user-read-playback-state user-modify-playback-state',
           code_challenge_method: 'S256',
           code_challenge,
           redirect_uri,
