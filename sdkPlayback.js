6af2c576de42ee3a13137cf60982f9e7a8e5164b
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-28 14:57:34 +0100

    Update sdkPlayback.js

--- sdkPlayback.js
+++ sdkPlayback.js
@@ -5,29 +5,30 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     getOAuthToken: cb => { cb(token); },
     volume: 0.5
   });
-// Ready
-player.addListener('ready', ({ device_id }) => {
-  console.log('Ready with Device ID', device_id);
-});
-
-// Not Ready
-player.addListener('not_ready', ({ device_id }) => {
-  console.log('Device ID has gone offline', device_id);
-});
-
-  player.addListener('initialization_error', ({ message }) => {
-    console.error(message);
-});
-
-player.addListener('authentication_error', ({ message }) => {
-    console.error(message);
-});
-
-player.addListener('account_error', ({ message }) => {
-    console.error(message);
-});
-
-player.connect();
-document.getElementById('play-pause').onclick = function() {
-  player.togglePlay();
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+  });
+  
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+  
+    player.addListener('initialization_error', ({ message }) => {
+      console.error(message);
+  });
+  
+  player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+  });
+  
+  player.addListener('account_error', ({ message }) => {
+      console.error(message);
+  });
+  
+  player.connect();
+  document.getElementById('play-pause').onclick = function() {
+    player.togglePlay();
+  };
 };
