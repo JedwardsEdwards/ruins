dc9be7dfde43f5da0bfca48133eb36778b7f1fba
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-09-28 14:58:59 +0100

    Update sdkPlayback.js

--- sdkPlayback.js
+++ sdkPlayback.js
@@ -1,5 +1,5 @@
 window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = '[My access token]';
+  const token = access_token;
   const player = new Spotify.Player({
     name: 'Web Playback SDK Quick Start Player',
     getOAuthToken: cb => { cb(token); },
