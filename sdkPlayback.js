6bc077e876cf8f2ed9d3c7f6a3e8a5e523e26412
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-17 11:06:12 +0100

    Update sdkPlayback.js

--- sdkPlayback.js
+++ sdkPlayback.js
@@ -17,38 +17,36 @@ function transferPlayback(id) {
 };
 
 window.onSpotifyWebPlaybackSDKReady = () => {
- console.log("onSpotifyWebPlaybackSDKReady called");
-  const token = access_token;
-  const player = new Spotify.Player({
-    name: 'Web Playback SDK Quick Start Player',
-    getOAuthToken: cb => { cb(token); },
-    volume: 0.5
-  });
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-    transferPlayback(device_id);
-  });
-  
-  // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-  });
-  
+    console.log("onSpotifyWebPlaybackSDKReady called");
+    const token = access_token;
+    const player = new Spotify.Player({
+        name: 'Web Playback SDK Quick Start Player',
+        getOAuthToken: cb => { cb(token); },
+        volume: 0.5
+        });
+    
+    console.log("adding listeners");
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+        transferPlayback(device_id);
+        });
+    player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+        });
     player.addListener('initialization_error', ({ message }) => {
+        console.error(message);
+        });
+    player.addListener('authentication_error', ({ message }) => {
       console.error(message);
-  });
-  
-  player.addListener('authentication_error', ({ message }) => {
+        });
+    player.addListener('account_error', ({ message }) => {
       console.error(message);
-  });
-  
-  player.addListener('account_error', ({ message }) => {
-      console.error(message);
-  });
-  
-  player.connect();
-  document.getElementById('play-pause').onclick = function() {
+        });
+    
+    console.log("attempting connection");
+    player.connect();
+    document.getElementById('play-pause').onclick = function() {
     player.togglePlay();
-  };
-};
+    };
+    console.log("done with OnSpotifyWebPlaybackSDKReady");
+    };
