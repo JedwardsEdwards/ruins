b88b13bc83dfd89d4d49798bac6d1ee741bcbe66
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-17 11:47:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ if (code) {
   });
 
   getUserData();
+  window.onSpotifyWebPlaybackSDKReady = onSpotifyWebPlaybackSDKReady
 } else {
   // we are not logged in so show the login button
   document.getElementById('login').style.display = 'unset';
