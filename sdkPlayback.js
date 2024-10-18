0e281e857b6b73664c9ce9a8c93323816c2b97be
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-18 13:08:49 +0100

    Update sdkPlayback.js

--- sdkPlayback.js
+++ sdkPlayback.js
@@ -123,7 +123,7 @@ function initSpotifyPlayer() {
                 player_loaded = true;
             };
             if (current_page == "loading") {
-                toMix();
+                transitionToPage(current_target);
             };
             updatePlayer(current_track);
         });
