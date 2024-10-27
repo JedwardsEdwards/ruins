da1bee44f4812252e4aa3b58a2befc94dc55bd83
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-27 20:32:39 +0000

    Update and rename hor_1 to hor_1.js

---
+++ hor_1.js
@@ -0,0 +1,21 @@
+function renderMixName() {
+  const page = document.getElementById("mix-page");
+  page.style.grid-template-rows: 20% 50% 30%;
+
+  const firstRow = document.createElement("div");
+  firstRow.id = "first-row";
+  firstRow.innerHTML = "first row text";
+
+  const secondRow = document.createElement("div");
+  secondRow.id = "first-row";  
+  secondRow.innerHTML = "second row text";
+
+  
+  const thirdRow = document.createElement("div");
+  thirdRow.id = "first-row";
+  thirdRow.innerHTML = "third row text";
+  
+  page.appendChild(firstRow);
+  page.appendChild(secondRow);
+  page.appendChild(thirdRow);
+};
