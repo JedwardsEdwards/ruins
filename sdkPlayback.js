73df56fb809b3fc689483d4701543471a21e9dcb
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-27 19:46:18 +0000

    Update sdkPlayback.js

--- sdkPlayback.js
+++ sdkPlayback.js
@@ -155,7 +155,8 @@ function initSpotifyPlayer() {
         //track_window: { current_track }
           //  }) => {
             stateObj) => {
-            if (!stateObj["loading"]) {
+            console.log(stateObj);
+            if (stateObj["loading"]) {
                 if (!window.player_loaded) {
                     window.player_loaded = true;
                 };
