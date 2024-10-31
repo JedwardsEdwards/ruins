5a06d9344c836cebbd9fe8fa289dc2853c17f163
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-31 20:36:42 +0000

    Update hor_1.js

--- hor_1.js
+++ hor_1.js
@@ -1,8 +1,9 @@
 function renderMixName() {
-  const page = document.getElementById("mix-page");
+  const container = document.getElementById("mix-name");
   // need some way to clear up the page here - remove old styles / kill child elements
   // probably need to do this before moving to the page
-  page.style["grid-template-rows"] = "20% 50% 30%";
+  
+  container.style["grid-template-rows"] = "20% 50% 30%";
 
   const firstRow = document.createElement("div");
   firstRow.id = "first-row";
@@ -18,7 +19,7 @@ function renderMixName() {
   thirdRow.innerHTML = "third row text";
 
   // could try css animations, don't know how they play when appending but might be easier??
-  setTimeout(() => {page.appendChild(firstRow)}, 1000);
-  setTimeout(() => {page.appendChild(secondRow)}, 2000);
-  setTimeout(() => {page.appendChild(thirdRow)}, 3000);
+  setTimeout(() => {container.appendChild(firstRow)}, 1000);
+  setTimeout(() => {container.appendChild(secondRow)}, 2000);
+  setTimeout(() => {container.appendChild(thirdRow)}, 3000);
 };
