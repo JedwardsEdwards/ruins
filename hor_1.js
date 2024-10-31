54c6d2079537fb27b93d5b05cb0f06661996e0ee
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-10-31 21:27:37 +0000

    Update hor_1.js

--- hor_1.js
+++ hor_1.js
@@ -19,26 +19,32 @@ function renderMixName() {
   dj_div.innerHTML = "DJ Incredibly Annoying";
   dj_div.style.height = "100%";
   dj_div.style.width = "100%"
+  dj_div.style.color = "white";
   firstRow.appendChild(dj_div);
   textFit(dj_div);
 
   const presents_div = document.createElement("div");
   presents_div.id = "mix-name-text-presents";
   presents_div.innerHTML = "presents";
+  presents_div.style.height = "100%";
+  presents_div.style.width = "100%"
+  presents_div.style.color = "white";
   firstRow.appendChild(presents_div);
-  //textFit(presents_div);
+  textFit(presents_div);
   
   const secondRow = document.createElement("div");
   secondRow.id = "second-row";  
   secondRow.innerHTML = "second row text";
-
+  //firstRow.style["grid-template-columns"] = "70% 30%";
+  container.appendChild(secondRow);
+  
   const thirdRow = document.createElement("div");
   thirdRow.id = "third-row";
   thirdRow.innerHTML = "third row text";
+  container.appendChild(thirdRow);
 
   // could try css animations, don't know how they play when appending but might be easier??
   container.appendChild(firstRow);
-  //setTimeout(() => {firstRow.appendChild(}, 1000);
-  setTimeout(() => {container.appendChild(secondRow)}, 2000);
-  setTimeout(() => {container.appendChild(thirdRow)}, 3000);
+  setTimeout(() => {dj_div.style.color = "black"}, 1000);
+  setTimeout(() => {presents_div.style.color = "black"}, 2000);
 };
