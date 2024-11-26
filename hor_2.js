999e403bb1e777797cd754c220d118d02e5f6f1d
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 13:30:36 +0000

    Update hor_2.js

--- hor_2.js
+++ hor_2.js
@@ -1,10 +1,40 @@
 function renderRancid() {
   info("renderRancid", "called");
   const container = document.getElementById("mix-name");
+  
+  container.style["grid-template-rows"] = "30% 50% 20%";
+  
+  const firstRow = createRow("first-row", row_styles);
+  firstRow.style["grid-template-columns"] = "30% 10% 50% 10%";
+  container.appendChild(firstRow);
+  const hour_div = createElement("hour", "1 HOUR", hidden_styles);
+  appendAndFit(firstRow,hour_div);
+  const and_div = createElement("and", "AND", hidden_styles);
+  appendAndFit(firstRow,and_div);
+  const min_div = createElement("min", "58 MINUTES", hidden_styles);
+  appendAndFit(firstRow,min_div);
+  const of_div = createElement("of", "OF", hidden_styles);
+  appendAndFit(firstRow,of_div);
 
-  // clean up from last run - probably need to do this before moving to the page
-  // also need to check how many times this render is run depending on where it is 
-  container.innerHTML = "";
-  container.style = "";};
+  const secondRow = createRow("second-row", "");
+  container.appendChild(secondRow);
+  const rancid_div = createElement("rancid", "RANCID TECHNO", hidden_styles);
+  appendAndFit(firstRow,rancid_div);
+  
+  const thirdRow = createRow("third-row", "");
+  container.appendChild(thirdRow);
+  const courtesy_div = createElement("courtesy", "COURTESY OF", hidden_styles);
+  appendAndFit(firstRow,courtesy_div);
+  const syd_div = createElement("syd", "SYD", hidden_styles);
+  appendAndFit(firstRow,syd_div);
+
+  setTimeout(() => {hour_div.style.color = "inherit"}, 1000);
+  setTimeout(() => {and_div.style.color = "inherit"}, 2000);
+  setTimeout(() => {min_div.style.color = "inherit"}, 3000);
+  setTimeout(() => {of_div.style.color = "inherit"}, 4000);
+  setTimeout(() => {rancid_div.style.color = "inherit"}, 5000);
+  setTimeout(() => {courtesy_div.style.color = "inherit"}, 6000);
+  setTimeout(() => {syd_div.style.color = "inherit"}, 7000);
+};
 
 mixRenderFunction["rancid"] = renderRancid;
