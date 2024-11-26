a51b1c2010b1743873a8f0241f3241f7aa9b7f23
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 15:51:07 +0000

    Update hor_2.js

--- hor_2.js
+++ hor_2.js
@@ -7,28 +7,28 @@ function renderRancid() {
   const firstRow = createRow("first-row", row_styles);
   firstRow.style["grid-template-columns"] = "35% 10% 50% 5%";
   container.appendChild(firstRow);
-  const hour_div = createElement("hour", "1 HOUR", hidden_styles);
+  const hour_div = createElement("hour", "1 HOUR", getHiddenStyles());
   appendAndFit(firstRow,hour_div);
   const and_div = createElement("and", "AND", "height:80%; width:100%; color:white; text-align:center");
   appendAndFit(firstRow,and_div);
-  const min_div = createElement("min", "58 MINUTES", hidden_styles);
+  const min_div = createElement("min", "58 MINUTES", getHiddenStyles());
   appendAndFit(firstRow,min_div);
   const of_div = createElement("of", "OF", "height:80%; width:100%; color:white; text-align:center");
   appendAndFit(firstRow,of_div);
 
   const secondRow = createRow("second-row", "");
   container.appendChild(secondRow);
-  const rancid_div = createElement("rancid", "RANCID", hidden_styles);
+  const rancid_div = createElement("rancid", "RANCID", getHiddenStyles());
   appendAndFit(secondRow,rancid_div);
   
   const thirdRow = createRow("third-row", "");
   container.appendChild(thirdRow);
-  const techno_div = createElement("techno", "TECHNO", hidden_styles);
+  const techno_div = createElement("techno", "TECHNO", getHiddenStyles());
   appendAndFit(thirdRow,techno_div);
    
   const fourthRow = createRow("fourth-row", "");
   container.appendChild(fourthRow);
-  const courtesy_div = createElement("courtesy", "COURTESY OF SYD.", hidden_styles);
+  const courtesy_div = createElement("courtesy", "COURTESY OF SYD.", getHiddenStyles());
   appendAndFit(fourthRow,courtesy_div);
 
   setTimeout(() => {hour_div.style.color = "inherit"}, 1000);
