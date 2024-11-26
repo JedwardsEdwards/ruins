29898fc53f6193bcb106d8866f527631d0b5b3e7
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 12:25:00 +0000

    Update hor_1.js

--- hor_1.js
+++ hor_1.js
@@ -110,9 +110,9 @@ function renderMixName() {
   setTimeout(() => {presents_div.style.color = "inherit"}, 2000);
 
   setTimeout(() => {sweaty.style.color = "inherit"}, 4000);
-  setTimeout(() => {firstHeart.style.color = "inherit"}, 5000);
-  setTimeout(() => {sticky.style.color = "inherit"}, 6000);
-  setTimeout(() => {slimy.style.color = "inherit"}, 8000);
+  setTimeout(() => {firstHeart.style.color = "inherit"}, 4500);
+  setTimeout(() => {sticky.style.color = "inherit"}, 5000);
+  setTimeout(() => {slimy.style.color = "inherit"}, 6000);
 
   /*
   setTimeout(() => {firstBracket.style.color = "inherit"}, 3000);
