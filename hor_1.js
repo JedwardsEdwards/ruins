9d4abdd940a8bb5e29b50f4821b38fc14b92c926
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 13:12:06 +0000

    Update hor_1.js

--- hor_1.js
+++ hor_1.js
@@ -1,37 +1,8 @@
-const mixRenderFunction = {};
 
-function createElement(id, text, styles) {
-   const element = document.createElement("div");
-   element.id = "hor-1-" + id;
-   element.innerHTML = text;
-   element.style = styles;
-   return element;
-};
-
-const hidden_styles = "height:100%; width:100%; color:white; text-align:center";
-
-function appendAndFit(parent, child) {
-  parent.appendChild(child);
-  textFit(child,  {multiLine : false});
-};
-
-function createRow(id, styles) {
-   const row = document.createElement("div");
-   row.id = id;
-   row.style = styles;
-   return row;
-};
-
-const row_styles = "display: grid; align-items: end";
 
 function renderSlimy() {
   info("renderSlimy", "called");
   const container = document.getElementById("mix-name");
-
-  // clean up from last run - probably need to do this before moving to the page
-  // also need to check how many times this render is run depending on where it is 
-  container.innerHTML = "";
-  container.style = "";
   
   container.style["grid-template-rows"] = "30% 40% 15% 15%";
   //container.style["grid-template-rows"] = "20% 30% 50%";
