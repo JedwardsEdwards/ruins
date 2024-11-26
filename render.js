1c3fb45d7a76657741ec3edb6460d93a31215c1f
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 13:36:17 +0000

    Update render.js

--- render.js
+++ render.js
@@ -25,10 +25,12 @@ function createRow(id, styles) {
 const row_styles = "display: grid; align-items: end";
 
 function clearRender() {
-  const container = document.getElementById("mix-name");
-
-  // clean up from last run - probably need to do this before moving to the page
-  // also need to check how many times this render is run depending on where it is 
+  let container = document.getElementById("mix-name");
   container.innerHTML = "";
   container.style = "";
+   
+  let name = document.getElementById('track-name');
+  let artist = document.getElementById('track-artist');
+  name.innerHTML= "";
+  artist.innerHTML= "";
 };
