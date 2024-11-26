3401d20f72f4c1f329423e6fdad6e3d0823b04f0
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 12:54:58 +0000

    Update hor_2.js

--- hor_2.js
+++ hor_2.js
@@ -1,5 +1,10 @@
 function renderRancid() {
-  console.log("rendering rancid name now);
-};
+  info("renderRancid", "called");
+  const container = document.getElementById("mix-name");
+
+  // clean up from last run - probably need to do this before moving to the page
+  // also need to check how many times this render is run depending on where it is 
+  container.innerHTML = "";
+  container.style = "";};
 
 mixRenderFunction["rancid"] = renderRancid;
