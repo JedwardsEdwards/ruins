aecbf2031708b8c3b09aa98b5ecf30bb9c2bf700
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-11-26 18:14:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -104,9 +104,9 @@ function setPalette(name, hide) {
   document.getElementsByTagName("html")["0"].style["color"] = display_palettes[name]["color"];
   document.getElementsByTagName("html")["0"].style["background-color"] = display_palettes[name]["background-color"];
   document.querySelectorAll(".button").forEach(b => {
-    b.style.transition = "none";
+    //b.style.transition = "none";
     b.style["background-color"] = display_palettes[name]["background-color"];
-    b.style.transition = "background-color 0.5s";
+    //b.style.transition = "background-color 0.5s";
     b.addEventListener("mouseenter", function( event ) {event.target.style["background-color"] = display_palettes[name]["hover"]}, false);
     b.addEventListener("mouseleave", function( event ) {event.target.style["background-color"]  = display_palettes[name]["background-color"]}, false);
     b.addEventListener("click", function( event ) {event.target.style["background-color"]  = display_palettes[name]["highlight-color"]}, false)
