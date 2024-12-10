7777c2dd338b6353022a9be4bf0d8bb5f647fbbc
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2024-12-10 18:09:40 +0000

    Update textFit.js

--- textFit.js
+++ textFit.js
@@ -183,8 +183,8 @@
     if (settings.alignVert) {
       addStyleSheet();
       console.log("TEST TEXT FIT | scroll height: " + innerSpan.scrollHeight );
-      //var height = innerSpan.scrollHeight;
-      var height = Math.min(innerSpan.scrollHeight,originalHeight);
+      var height = innerSpan.scrollHeight;
+      //var height = Math.min(innerSpan.scrollHeight,originalHeight);
       if (window.getComputedStyle(el)['position'] === "static"){
         el.style['position'] = 'relative';
       }
